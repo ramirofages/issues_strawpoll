7baa03f41fc55c8ddc8de0d57926cbb231e82f0a
Ramiro Fages <fages_fages@hotmail.com>
2016-07-15 23:58:08 -0300

    Added test for project's polymorphic association

--- app/models/mocked_source.rb
+++ app/models/mocked_source.rb
@@ -1,4 +1,7 @@
 class MockedSource < ActiveRecord::Base
 	has_one :project, as: :project_source
 
+	def retrieve_issues
+		MockedIssue.all
+	end
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	setup do
+		@project = Project.new
+	end
+
+  test "polymorphic association" do
+  	
+  	@project.project_source = MockedSource.new
+
+  	assert_difference('Project.count') do
+			@project.save
+		end
+
+		assert_nothing_raised do
+  		created_project = Project.all.last.project_source.retrieve_issues
+		end
+
+  end
+
 end
