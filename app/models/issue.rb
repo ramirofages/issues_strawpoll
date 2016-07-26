71127bd55317bfcbba24d5662af1a84b6727ca62
Ramiro Fages <fages_fages@hotmail.com>
2016-07-25 23:46:49 -0300

    Added tests and validations for issue

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,9 @@
 class Issue < ActiveRecord::Base
+
+  validates :poll_id, :src_issue_id, :name, :description, presence: true
+
+
+
   belongs_to :poll
   has_many :votes
 end
--- test/models/issue_test.rb
+++ test/models/issue_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class IssueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "should create issue" do
+
+		issue = Issue.new do |issue|
+			issue.poll = polls(:without_issues)
+			issue.src_issue_id = 4
+			issue.name = "asd"
+			issue.description = "asdasd"
+		end
+
+		assert_difference('Issue.count') do
+			issue.save
+		end
+	end
+
+	test "should not create issue without src_id" do
+
+		issue = Issue.new do |issue|
+			issue.poll = polls(:without_issues)
+			issue.name = "asd"
+			issue.description = "asdasd"
+		end
+
+		assert_no_difference('Issue.count') do
+			issue.save
+		end
+	end
+
+	test "should not create issue without poll" do
+
+		issue = Issue.new do |issue|
+			issue.name = "asd"
+			issue.src_issue_id = 4
+			issue.description = "asdasd"
+		end
+
+		assert_no_difference('Issue.count') do
+			issue.save
+		end
+	end
 end
