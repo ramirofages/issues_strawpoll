385930d1a34065ec8a727568229eee4b25108fe1
Ramiro Fages <fages_fages@hotmail.com>
2016-07-27 05:00:59 -0300

    Added test for project source controller

--- test/controllers/mocked_sources_controller_test.rb
+++ test/controllers/mocked_sources_controller_test.rb
@@ -1,9 +1,34 @@
 require 'test_helper'
 
-class MockedSourcesControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
+class User::Sources::MockedSourcesControllerTest < ActionController::TestCase
+  	
+  include Devise::Test::ControllerHelpers
+
+  test "should get new on project without source" do
+
+    sign_in users(:one)
+
+    get :new, project_id: projects(:without_source)
+
     assert_response :success
+  end
+
+  test "should not get new on project with source" do
+
+    sign_in users(:one)
+
+    get :new, project_id: projects(:with_source)
+
+    assert_response :redirect
+  end
+
+  test "should not get new on unowned project" do
+
+    sign_in users(:three)
+
+    get :new, project_id: projects(:without_source)
+
+    assert_response :redirect
   end
 
 end
