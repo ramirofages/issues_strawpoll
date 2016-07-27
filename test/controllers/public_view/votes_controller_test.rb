c85523bb513f25f58e0c7945ec3fea3e4450b1c4
Ramiro Fages <fages_fages@hotmail.com>
2016-07-27 04:39:24 -0300

    Added tests for votes controller

--- test/controllers/public_view/votes_controller_test.rb
+++ test/controllers/public_view/votes_controller_test.rb
@@ -1,8 +1,35 @@
 require 'test_helper'
 
 class PublicView::VotesControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
+  include Devise::Test::ControllerHelpers
+
+
+
+  test "should NOT get new when signed out" do
+		poll = polls(:from_project_one) 
+
+    get :new, project_id: poll.project, poll_id: poll
+
+    assert_response :redirect
+  end
+
+  test "should get new when signed in as the owner of the poll" do
+  	sign_in users(:one)
+
+		poll = polls(:from_project_one) 
+
+    get :new, project_id: poll.project, poll_id: poll
+
+    assert_response :success
+  end
+
+  test "should get new when signed in as another user" do
+  	sign_in users(:three)
+
+		poll = polls(:from_project_one) 
+
+    get :new, project_id: poll.project, poll_id: poll
+
     assert_response :success
   end
 
